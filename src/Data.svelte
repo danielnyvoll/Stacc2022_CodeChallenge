b827a3dd78e5096d399b3bf23ec39a754ccfc9de
danielnyvoll <danielnyvoll@hotmail.com>
2022-03-18 21:47:25 +0100

    Search on company org number, employees and names

--- src/Data.svelte
+++ src/Data.svelte
@@ -1,80 +1,77 @@
 <script>
-let listen = [];
-let personData = [];
+import { onMount } from 'svelte';
 let companyData = [];
 let personInCompanyData = [];
+let personHits;
+let job;
+let bedrift;
+let companyNumber= [];
 
 
-async function info() {
-        const res = await fetch(`https://data.brreg.no/enhetsregisteret/api/enheter`);
-        const data = await res.json()
-        console.log(data)
-        for(var i = 0; i < data.length; i++) {
-            var obj = json[i];
-            console.log(obj.id);
-        }
-        personInCompanyData = data
-    }
+async function searchCompany() {
+        const res = await fetch(`https://data.brreg.no/enhetsregisteret/api/enheter?navn=${bedrift}&konkurs=false`);
+		const data = await res.json();
+        companyNumber = data._embedded.enheter;
+        console.log(companyNumber);
+}
 
-
-async function getInfoAboutRoles() {
+async function searchEmployees() {
         const res = await fetch(`https://data.brreg.no/enhetsregisteret/api/enheter/${nummer}/roller`);
-        const data = await res.json()
-        console.log(data)
-        personInCompanyData = data
-    }
+		const data = await res.json();
+        companyNumber = data.rollegrupper;
+        console.log(companyNumber);
+}
 
-async function getInfoAboutCompany() {
-        const res = await fetch(`https://code-challenge.stacc.dev/api/roller?orgNr=${nummer}`);
-        const data = await res.json()
-        console.log(data)
-        return data
-    }
-
-async function getInfoAboutPeopleInCompany() {
-        const res = await fetch(`https://code-challenge.stacc.dev/api/pep?name=${nummer}`);
-        const data = await res.json()
-        console.log(data)
-        return data
-    }
 
 async function getName() {
         const res = await fetch(`https://code-challenge.stacc.dev/api/pep?name=${name}`);
         const data = await res.json()
         console.log(data)
         hits = data.numberOfHits
+        personHits = data.hits[0].aliases
+        job = data.hits[0].dataset
+        console.log(job)
+        console.log(personHits)
         console.log(hits)
-        return data
     }
 
 let name = "";
-let nummer = "";
-let hits = 0;
+let hits="";
+let nummer;
+let sjef;
 </script>
 
 <main>
-    <button id="search" on:click={info}>Search</button>
-    <p>988971375</p>
-    <input type="search" name="company" bind:value={nummer} placeholder="">
-    <button id="search" on:click={getInfoAboutRoles}>Search</button>
-    {#each companyData as roller}
-        <p>{roller}</p>
+    <p>Finn org nummer</p>
+    <input type="search" name="name" bind:value={bedrift} placeholder="Search for a company">
+    <button id="search" on:click={searchCompany}>Search</button>
+    {#each companyNumber as match}
+        <p>{match.navn} med org nummer: {match.organisasjonsnummer}</p>    
     {/each}
+
+<div>
+    <input type="search" name="name" bind:value={nummer} placeholder="org:nummer">
+    <button id="search" on:click={searchEmployees}>Search</button>
+    {#each companyNumber as roles}
+    {console.log(roles)}
+        {#each roles.roller as ansatt}
+            {name = ansatt.person.navn.etternavn}
+            {getName}
+            <p>{name}</p>
+        {/each}
+    {/each}
+</div>
+
+
     <div>
         <p>Search for a person. Example: Knut Arild Hareide</p>
     <input type="search" name="name" bind:value={name} placeholder="Search for a name..">
     <button id="search" on:click={getName}>Search</button>
     
     {#if hits > 0}
-        <p>This person is flagged</p>
+        <p>{personHits} er flagget</p>
     {:else}
-        <p>{hits}</p>
+        <p>Personen du søkte etter er ikke flagget</p>
     {/if}
-    </div>
-
-    <div>
-        <p>Search for a Company by number. Example: 981078365</p>
-        <input type="search" name="company" bind:value={nummer} placeholder="Search for a company by number..">
-        <button on:click={() => companyData = getInfoAboutCompany()}>Search</button>
     </div>
 </main>
